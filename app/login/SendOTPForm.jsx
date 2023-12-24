35137356da0f7d9aaf56b854d0809f3df62072b5
mahdiyar <mahdiyar472@gmail.com>
2023-12-25 02:47:52 +0330

    fix otp form style and codes

--- app/login/SendOTPForm.jsx
+++ app/login/SendOTPForm.jsx
@@ -3,18 +3,20 @@ import Button from "@/common/Button";
 import TextField from "@/common/TextField";
 import React from "react";
 
-const SendOTPForm = ({ value, onChange, onClick }) => {
+const SendOTPForm = ({ value, onChange, onClick, onSubmit }) => {
   return (
-    <form className="sm:max-w-sm p-2 w-full">
+    <form className="sm:max-w-sm p-2 w-full" onSubmit={onSubmit}>
+      <h2 className="font-bold text-2xl text-center mb-10">ورود</h2>
       <TextField
         name="phoneNumber"
         label="شماره همراه"
         value={value}
         type="tel"
         onChange={onChange}
-        placeholder="09*********"
+        maxLength={11}
+        placeholder="۰۹*********"
       />
-      <Button className="mt-3" name="دریافت کد" onClick={onClick} />
+      <Button className="mt-3 w-full" name="دریافت کد" type="submit" />
     </form>
   );
 };
