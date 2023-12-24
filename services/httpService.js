c89e27f475e86a2d9059b4826d24a384922b0929
mahdiyar <mahdiyar472@gmail.com>
2023-12-25 02:48:28 +0330

    add axios and confgure it

---
+++ services/httpService.js
@@ -0,0 +1,15 @@
+const { default: axios } = require("axios");
+
+const app = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  withCredentials: true,
+});
+
+const http = {
+  get: app.get,
+  post: app.post,
+  patch: app.patch,
+  put: app.put,
+};
+
+export default http;
