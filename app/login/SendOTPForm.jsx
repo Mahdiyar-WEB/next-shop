349c6382f79c0a8f3cee3193786568adc7b6181a
mahdiyar <mahdiyar472@gmail.com>
2023-12-26 19:54:15 +0330

    add loader to sendotp form

--- app/login/SendOTPForm.jsx
+++ app/login/SendOTPForm.jsx
@@ -3,7 +3,7 @@ import Button from "@/common/Button";
 import TextField from "@/common/TextField";
 import React from "react";
 
-const SendOTPForm = ({ value, onChange, onClick, onSubmit }) => {
+const SendOTPForm = ({ value, onChange, onSubmit, isPending }) => {
   return (
     <form className="sm:max-w-sm p-2 w-full" onSubmit={onSubmit}>
       <h2 className="font-bold text-2xl text-center mb-10">ورود</h2>
@@ -16,7 +16,22 @@ const SendOTPForm = ({ value, onChange, onClick, onSubmit }) => {
         maxLength={11}
         placeholder="۰۹*********"
       />
-      <Button className="mt-3 w-full" name="دریافت کد" type="submit" />
+      <Button
+        className="mt-4 w-full"
+        name={
+          isPending ? (
+            <div
+              class="flex mx-auto h-6 w-6 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+              role="status"
+            >
+              <span class="!-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"></span>
+            </div>
+          ) : (
+            "دریافت کد تایید"
+          )
+        }
+        type="submit"
+      />
     </form>
   );
 };
