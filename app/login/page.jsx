610b4795eece0418249d1b711a0419b2c5e68e73
mahdiyar <mahdiyar472@gmail.com>
2024-01-02 23:41:24 +0330

    add timer for check otp form

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SendOTPForm from "./SendOTPForm";
 import toPersianDigits from "@/utils/toPersianDigits";
 import toEnglishDigits from "@/utils/toEnglishDigits";
@@ -7,11 +7,16 @@ import { toast } from "react-hot-toast";
 import { useMutation } from "@tanstack/react-query";
 import { getOTP, checkOTP } from "@/services/authServices";
 import CheckOTPForm from "./CheckOTPForm";
+import { useRouter } from "next/navigation";
+
+const RESEND_TIMER = 60;
 
 const Login = () => {
   const [step, setStep] = useState(1);
   const [phoneNumber, setPhoneNumber] = useState("");
   const [otp, setOtp] = useState("");
+  const [timer, setTimer] = useState(null);
+  const router = useRouter();
   const {
     data: getOTPData,
     error: getOTPError,
@@ -33,6 +38,7 @@ const Login = () => {
     const digits = toPersianDigits(e.target.value);
     setPhoneNumber(digits);
   };
+
   const handleSendOTP = async (e) => {
     e.preventDefault();
     try {
@@ -43,17 +49,21 @@ const Login = () => {
     } catch (error) {
       toast.error(error?.response?.data?.message);
       setStep(2);
+      setTimer(RESEND_TIMER);
     }
   };
+
   const handleValidateOTP = async (e) => {
     e.preventDefault();
     try {
       const convertedPhoneNumber = toEnglishDigits(phoneNumber);
       const convertedOtp = toEnglishDigits(otp);
-      const { data } = await checkOTPMutateAsync({
+      const { user, message } = await checkOTPMutateAsync({
         phoneNumber: convertedPhoneNumber,
         otp: convertedOtp,
       });
+      toast.success(message);
+      user?.isActive ? router.push("/") : router.push("/edit-profile");
     } catch (error) {
       toast.error(error?.response?.data?.message);
     }
@@ -63,6 +73,20 @@ const Login = () => {
     const digits = toPersianDigits(e);
     setOtp(digits);
   };
+
+  const handleEditNumber = () => {
+    setStep(1);
+    setPhoneNumber("");
+  };
+
+  useEffect(() => {
+    const timerInterval =
+      timer > 0 && setInterval(() => setTimer((t) => t - 1), 1000);
+
+    return () => {
+      timerInterval && clearInterval(timerInterval);
+    };
+  }, [timer]);
 
   const renderSteps = () => {
     switch (step) {
@@ -82,6 +106,9 @@ const Login = () => {
             otp={otp}
             onChange={handleOTPChange}
             isPending={checkOTPIsPending}
+            editNumber={handleEditNumber}
+            timer={timer}
+            resendCode={handleSendOTP}
           />
         );
     }
