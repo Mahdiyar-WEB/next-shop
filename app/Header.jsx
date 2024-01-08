591bd5c2744527b5d1dce753b3c763d35389f080
mahdiyar <mahdiyar472@gmail.com>
2024-01-08 19:02:51 +0330

    add cart and loading for login link

--- app/Header.jsx
+++ app/Header.jsx
@@ -1,7 +1,11 @@
+"use client";
+import useAuth from "@/hooks/useAuth";
 import Link from "next/link";
 import React from "react";
 
 const Header = () => {
+  const { isLoading, data, error } = useAuth();
+
   return (
     <header className="shadow-md mb-10">
       <ul className="container xl:max-w-screen-xl flex py-4 px-6 lg:px-10 justify-between">
@@ -12,8 +16,24 @@ const Header = () => {
           <Link href="/products">محصولات</Link>
         </li>
         <li>
-          <Link href="/login">ورود</Link>
+          <Link href="/cart">سبد خرید</Link>
         </li>
+        {isLoading ? (
+          <div
+            className="flex h-6 w-6 animate-spin rounded-full text-primary-900 border-2 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status"
+          >
+            <span className="!-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"></span>
+          </div>
+        ) : (
+          <li>
+            {data && !error ? (
+              <Link href="/profile">{data.user.name}</Link>
+            ) : (
+              <Link href="/login">ورود</Link>
+            )}
+          </li>
+        )}
       </ul>
     </header>
   );
