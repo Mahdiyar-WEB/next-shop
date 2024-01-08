05d795e7e9d6057f72ad0909931d50f487da26fc
mahdiyar <mahdiyar472@gmail.com>
2024-01-08 19:03:10 +0330

    change the placement of Provider for react query

--- app/layout.js
+++ app/layout.js
@@ -13,11 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className={`${vazirFont.variable} font-sans`}>
-        <Toaster />
-        <Header />
-        <main className="container xl:max-w-screen-xl">
-          <Providers>{children}</Providers>
-        </main>
+        <Providers>
+          <Toaster />
+          <Header />
+          <main className="container xl:max-w-screen-xl">{children}</main>
+        </Providers>
       </body>
     </html>
   );
