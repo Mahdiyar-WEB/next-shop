c150c9e0a5a3131ddbeeb481c64b3f77e5d3f8be
mahdiyar <mahdiyar472@gmail.com>
2024-01-08 19:15:00 +0330

    add inspectors for axios request and response to get accessToken when refreshToken was available

--- services/httpService.js
+++ services/httpService.js
@@ -5,6 +5,30 @@ const app = axios.create({
   withCredentials: true,
 });
 
+app.interceptors.request.use(
+  (res) => res,
+  (error) => Promise.reject(error)
+);
+
+app.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const errorConfig = error.config;
+    if (err.response.status === 401 && !errorConfig._retry) {
+      errorConfig._retry = true;
+      try {
+        const { data } = await axios.get(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/user/refresh-token`,
+          { withCredentials: true }
+        );
+        data && app(errorConfig);
+      } catch (error) {
+        Promise.reject(error);
+      }
+    }
+    Promise.reject(error);
+  }
+);
 const http = {
   get: app.get,
   post: app.post,
