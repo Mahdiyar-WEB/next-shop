da257f539f09547802d85164c8b301af84d24a35
mahdiyar <mahdiyar472@gmail.com>
2024-01-09 20:34:29 +0330

    add userAuthorization utils

---
+++ utils/userAuthorization.js
@@ -0,0 +1,25 @@
+const userAuthorization = async (request) => {
+  try {
+    let cookies = "";
+    request.cookies
+      .getAll()
+      .map(({ name = "", value = "" }) => (cookies += `${name}=${value} `));
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/user/profile`,
+      {
+        credentials: "include",
+        headers: {
+          Cookie: cookies,
+        },
+      }
+    );
+    const { data } = await response.json();
+    const { user } = data || {};
+    return user;
+  } catch (error) {
+    return null;
+  }
+};
+
+export default userAuthorization;
