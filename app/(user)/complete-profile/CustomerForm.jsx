6c39466ec08173509cee293dff41382c6411f299
mahdiyar <mahdiyar472@gmail.com>
2024-01-14 23:49:33 +0330

    change the placement of complete profile

--- app/(user)/complete-profile/CustomerForm.jsx
+++ app/(user)/complete-profile/CustomerForm.jsx
@@ -2,17 +2,11 @@ import Button from "@/common/Button";
 import TextField from "@/common/TextField";
 import React from "react";
 
-const CustomerForm = ({
-  onSubmit,
-  name,
-  email,
-  onChange,
-  isPending,
-}) => {
+const CustomerForm = ({ onSubmit, name, email, onChange, isPending }) => {
   return (
     <form className="sm:max-w-sm p-2 w-full" onSubmit={onSubmit}>
       <h2 className="font-bold text-2xl text-center mb-10">
-        ویرایش یا تکمیل اطلاعات
+        تکمیل اطلاعات
       </h2>
       <TextField
         name="name"
@@ -30,17 +24,13 @@ const CustomerForm = ({
         onChange={onChange}
         placeholder="example@gmail.com"
       />
-      <Button className="mt-4 w-full" color="primary" type="submit">
-        {isPending ? (
-          <div
-            className="flex mx-auto h-6 w-6 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-            role="status"
-          >
-            <span className="!-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"></span>
-          </div>
-        ) : (
-          "تایید"
-        )}
+      <Button
+        isPending={isPending}
+        className="mt-4 w-full"
+        color="primary"
+        type="submit"
+      >
+        تایید
       </Button>
     </form>
   );
