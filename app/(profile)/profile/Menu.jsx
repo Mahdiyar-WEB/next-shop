d8af2a411808fccd0f8393c784a5fb3a83ff99f9
mahdiyar <mahdiyar472@gmail.com>
2024-01-14 23:48:24 +0330

    develop menu for mobile size for user

---
+++ app/(profile)/profile/Menu.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+const Menu = ({ links }) => {
+  return (
+    <header className="fixed z-10 bottom-6 md:hidden left-0 right-0 border w-5/6 mx-auto px-4 py-3 rounded-lg shadow-md">
+      <ul className="flex justify-between">
+        {links.map(({ title, href }) => {
+          return (
+            <li key={title}>
+              <Link href={href}>{title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </header>
+  );
+};
+
+export default Menu;
