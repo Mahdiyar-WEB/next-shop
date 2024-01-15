67745f1ea65ccdde07363c9560bf6f3407b78271
mahdiyar <mahdiyar472@gmail.com>
2024-01-15 20:20:09 +0330

    update menu

--- app/(profile)/profile/Menu.jsx
+++ app/(profile)/profile/Menu.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Menu = ({ links }) => {
   return (
-    <header className="fixed z-10 bottom-6 md:hidden left-0 right-0 border w-5/6 mx-auto px-4 py-3 rounded-lg shadow-md">
+    <header className="fixed z-10 bottom-6 md:hidden bg-white left-0 right-0 border w-5/6 mx-auto px-4 py-3 rounded-lg shadow-md">
       <ul className="flex justify-between">
         {links.map(({ title, href }) => {
           return (
