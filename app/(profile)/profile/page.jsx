17eb66cfd98235302ec782dafb58469ac0fff732
mahdiyar <mahdiyar472@gmail.com>
2024-01-29 10:57:13 +0330

    update profile

--- app/(profile)/profile/page.jsx
+++ app/(profile)/profile/page.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client";
+import useAuth from "@/hooks/useAuth";
+import { data } from "autoprefixer";
+import React from "react";
 
 const Profile = () => {
+  const { data: information } = useAuth();
   return (
-    <main className="mx-8 mt-7 grid grid-cols-12 gap-4">خوش آمدید</main>
-  )
-}
+    <main className="mx-8 mt-7 grid grid-cols-12 gap-4">
+      <div className="flex gap-1 whitespace-nowrap">
+        <span>خوش آمدید</span>
+        <span>{information?.data?.user?.name}</span>
+      </div>
+    </main>
+  );
+};
 
-export default Profile
+export default Profile;
