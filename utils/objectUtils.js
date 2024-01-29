c87ce896ffd53d427d64b098d3caa326d90062ef
mahdiyar <mahdiyar472@gmail.com>
2024-01-29 11:02:45 +0330

    add are valuesSame util

--- utils/objectUtils.js
+++ utils/objectUtils.js
@@ -1,8 +1,20 @@
 export const includeObject = (obj, includesKeys) => {
   const includedObject = {};
   Object.keys(obj)
-  .filter((key) => includesKeys.includes(key))
-  .forEach((key) => (includedObject[key] = obj[key]));
-  
+    .filter((key) => includesKeys.includes(key))
+    .forEach((key) => (includedObject[key] = obj[key] || ""));
+
   return includedObject;
+};
+export const areValuesSame = (obj1, obj2) => {
+  for (let key in obj1) {
+    if (obj1.hasOwnProperty(key) && obj2.hasOwnProperty(key)) {
+      if (obj1[key] !== obj2[key]) {
+        return false;
+      }
+    } else {
+      return false; // Keys don't match
+    }
+  }
+  return true; // All values match
 };
