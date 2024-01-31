d5e317163e4c1c397dccf30bcd59278a80e30a18
mahdiyar <mahdiyar472@gmail.com>
2024-01-31 12:18:10 +0330

    add Product component

---
+++ components/Product.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Product = ({
+  _id,
+  title,
+  description,
+  slug,
+  category,
+  imageLink,
+  price,
+  discount,
+  offPrice,
+  brand,
+  tags,
+  likesCount,
+  isLiked,
+  countInStock,
+  rating,
+  numReviews,
+  createdAt,
+  updatedAt,
+}) => {
+  return <div>Product</div>;
+};
+
+export default Product;
