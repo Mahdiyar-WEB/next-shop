315b1c3f9c129286068f5fa1657c76b6c75ef1bc
mahdiyar <mahdiyar472@gmail.com>
2024-01-31 12:18:54 +0330

    develop products page in user route group

--- app/(user)/products/page.jsx
+++ app/(user)/products/page.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Product from "@/components/Product";
+import { getProducts } from "@/services/productServices";
 
-const Products = () => {
+const Products = async ({ searchParams }) => {
+  const { data } = await getProducts(searchParams?.category || "");
+
   return (
-    <div>Products</div>
-  )
-}
+    <div className="border border-primary-800 grid-cols-12 w-full">
+      {data.products.map((product) => {
+        return <Product key={product._id} {...product} />;
+      })}
+    </div>
+  );
+};
 
-export default Products
+export default Products;
