377607214e9498a29d60082ee96861677428cf60
mahdiyar <mahdiyar472@gmail.com>
2024-02-11 03:24:07 +0330

    refactor getProducts http service handler

--- services/productServices.js
+++ services/productServices.js
@@ -1,8 +1,10 @@
 const { default: http } = require("./httpService");
 
-export const getProducts = async (category) => {
+export const getProducts = async (params) => {
+  const finalParams = Object.keys(params)?.map((paramKey) => {
+    return `${paramKey}=${params[paramKey]}`;
+  });
   return http
-    .get(`/product/list${category && `?category=${category}`}`)
+    .get(`/product/list?${finalParams.join("&")}`)
     .then((res) => res.data);
 };
-
