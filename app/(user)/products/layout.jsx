4b2cdfb8524530716182be5621eb0532c3b80162
mahdiyar <mahdiyar472@gmail.com>
2024-02-11 03:22:23 +0330

    update products layout

--- app/(user)/products/layout.jsx
+++ app/(user)/products/layout.jsx
@@ -1,11 +1,15 @@
 import Categories from "./Categories";
 import { getCategories } from "@/services/categoryServices";
+import Sort from "./Sort";
 
 export default async function RootLayout({ children }) {
   const { data } = await getCategories();
   return (
     <section className="flex flex-col lg:flex-row gap-5">
-      <Categories data={data} />
+      <aside >
+        <Categories data={data} />
+        <Sort />
+      </aside>
       {children}
     </section>
   );
