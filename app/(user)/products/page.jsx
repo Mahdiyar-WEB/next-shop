d3f4134593563099dd367e237fe7a4d012e943cf
mahdiyar <mahdiyar472@gmail.com>
2024-02-11 03:22:55 +0330

    edit getProducts calling

--- app/(user)/products/page.jsx
+++ app/(user)/products/page.jsx
@@ -1,11 +1,11 @@
 import Product from "@/components/Product";
 import { getProducts } from "@/services/productServices";
-export const dynamic = "force-dynamic" // this value makes this page SSR but if we use force-static it will be render as SSG
+export const dynamic = "force-dynamic"; // this value makes this page SSR but if we use force-static it will be render as SSG
 
 //** Parallel and sequential rendering in next course season 16 (217) */
 
 const Products = async ({ searchParams }) => {
-  const { data } = await getProducts(searchParams?.category || "");
+  const { data } = await getProducts(searchParams);
 
   return (
     <div className="border border-primary-800 grid-cols-12 w-full">
