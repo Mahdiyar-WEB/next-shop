9c691812155550fd5a1600bc912ab578bac6b5fb
mahdiyar <mahdiyar472@gmail.com>
2024-02-11 18:52:40 +0330

    add getProductBySlug http service and refactor getProducts

--- services/productServices.js
+++ services/productServices.js
@@ -2,9 +2,16 @@ const { default: http } = require("./httpService");
 
 export const getProducts = async (params) => {
   const finalParams = Object.keys(params)?.map((paramKey) => {
-    return `${paramKey}=${params[paramKey]}`;
+    return `${paramKey}=${
+      params[paramKey].startsWith(",")
+        ? params[paramKey].substring(1)
+        : params[paramKey]
+    }`;
   });
   return http
     .get(`/product/list?${finalParams.join("&")}`)
     .then((res) => res.data);
+};
+export const getProductBySlug = async (slug) => {
+  return http.get(`/product/slug/${slug}`).then((res) => res.data);
 };
