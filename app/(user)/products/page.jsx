2a491a09e6e77529941d2ce0eb69eff9d1824ef9
mahdiyar <mahdiyar472@gmail.com>
2024-02-11 18:50:57 +0330

    refactor products page

--- app/(user)/products/page.jsx
+++ app/(user)/products/page.jsx
@@ -1,18 +1,33 @@
 import Product from "@/components/Product";
 import { getProducts } from "@/services/productServices";
+import Sort from "./Sort";
+import Categories from "./Categories";
+import { getCategories } from "@/services/categoryServices";
+
 export const dynamic = "force-dynamic"; // this value makes this page SSR but if we use force-static it will be render as SSG
 
-//** Parallel and sequential rendering in next course season 16 (217) */
-
 const Products = async ({ searchParams }) => {
-  const { data } = await getProducts(searchParams);
+  const getProductsPromise = getProducts(searchParams);
+  const getCategoriesPromise = getCategories();
+
+  //** Parallel and sequential rendering in next course season 16 (217) */
+  const [{ data }, { data: categories }] = await Promise.all([
+    getProductsPromise,
+    getCategoriesPromise,
+  ]);
 
   return (
-    <div className="border border-primary-800 grid-cols-12 w-full">
-      {data.products.map((product) => {
-        return <Product key={product._id} {...product} />;
-      })}
-    </div>
+    <section className="flex flex-col lg:flex-row gap-5">
+      <aside>
+        <Sort />
+        <Categories data={categories} />
+      </aside>
+      <div className="gap-6 grid grid-cols-12 w-full">
+        {data.products.map((product) => {
+          return <Product key={product._id} {...product} />;
+        })}
+      </div>
+    </section>
   );
 };
 
