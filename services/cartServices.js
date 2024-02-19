f3c7ca138da6ec943ab2422c0a42a39d9f9d315c
mahdiyar <mahdiyar472@gmail.com>
2024-02-19 16:14:48 +0330

    add removeFromCart and deleteFromCart to cartServices

--- services/cartServices.js
+++ services/cartServices.js
@@ -3,3 +3,9 @@ const { default: http } = require("./httpService");
 export const addToCart = async (productId) => {
   return http.post("/cart/add", { productId }).then((res) => res.data);
 };
+export const removeFromCart = async (productId) => {
+  return http.post("/cart/remove", { productId }).then((res) => res.data);
+};
+export const deleteFromCart = async (productId) => {
+  return http.post("/cart/delete", { productId }).then((res) => res.data);
+};
