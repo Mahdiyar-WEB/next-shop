7d61c52946bba653a3f3402f6798bc9e48523679
mahdiyar <mahdiyar472@gmail.com>
2024-03-01 21:39:23 +0330

    add tableHeads constants

---
+++ constants/tableHeads.js
@@ -0,0 +1,30 @@
+export const userOrdersTableHead = [
+  {
+    id: 1,
+    label: "#",
+  },
+  {
+    id: 2,
+    label: "شماره فاکتور",
+  },
+  {
+    id: 3,
+    label: "توضیحات",
+  },
+  {
+    id: 4,
+    label: "محصول",
+  },
+  {
+    id: 5,
+    label: "مبلغ",
+  },
+  {
+    id: 6,
+    label: "تاریخ",
+  },
+  {
+    id: 7,
+    label: "وضعیت سفارش",
+  },
+];
