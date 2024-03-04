0110126670834389380a24a9297ce5847428288b
mahdiyar <mahdiyar472@gmail.com>
2024-03-04 19:37:48 +0330

    use toStringCookies in userAuthorization

--- utils/userAuthorization.js
+++ utils/userAuthorization.js
@@ -1,16 +1,13 @@
+import { toStringCookies } from "./toStringCookies";
+
 const userAuthorization = async (request) => {
   try {
-    let cookies = "";
-    request.cookies
-      .getAll()
-      .map(({ name = "", value = "" }) => (cookies += `${name}=${value}; `));
-
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_BASE_URL}/user/profile`,
       {
         credentials: "include",
         headers: {
-          Cookie: cookies.trimEnd(),
+          Cookie: toStringCookies(request.cookies),
         },
       }
     );
