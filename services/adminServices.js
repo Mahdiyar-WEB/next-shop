a7fa6c8338ca5c1e5e1ad225c9bd56e5a2ec698c
Mahdiyar marvi <mahdiyar472@gmail.com>
2024-04-27 14:51:49 +0330

    add getUserByID service

--- services/adminServices.js
+++ services/adminServices.js
@@ -3,3 +3,6 @@ const { default: http } = require("./httpService");
 export const getAllUsers = async () => {
   return http.get("/admin/user/list").then((res) => res.data);
 };
+export const getUserByID = async (id) => {
+  return http.get(`/admin/user/profile/${id}`).then((res) => res.data);
+};
