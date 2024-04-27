2a9fd1fea46b7d436b5b7bf5339e772136464908
Mahdiyar marvi <mahdiyar472@gmail.com>
2024-04-27 14:50:37 +0330

    add badge color

--- common/Badge.jsx
+++ common/Badge.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
-const Badge = ({ title, icon, className }) => {
+const Badge = ({ title, icon, className, color }) => {
+  const getColorClassName = (color) => {
+    const colorClasses = {
+      primary: "badge-primary",
+      info: "badge-info",
+      danger: "badge-danger",
+      success: "badge-success",
+      warning: "badge-warning",
+    };
+    return colorClasses[color] || "";
+  };
   return (
     <div
-      className={`border w-fit px-2 py-1 text-xs flex gap-[2px] items-center rounded-lg ${className}`}
+      className={`border w-fit px-2 py-1 flex gap-[2px] items-center rounded-lg ${className} ${getColorClassName(
+        color
+      )}`}
     >
       <span>{icon}</span>
       <span>{title}</span>
