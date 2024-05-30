78e1c673192e2bebc27a12a68e88caec72262838
Mahdiyar marvi <mahdiyar472@gmail.com>
2024-05-30 18:09:36 +0330

    send cookies to Products Table to fetch products

--- app/(admin)/admin/products/page.jsx
+++ app/(admin)/admin/products/page.jsx
@@ -1,10 +1,26 @@
+import Button from "@/common/Button";
 import ProductsTable from "@/components/ProductsTable";
+import { toStringCookies } from "@/utils/toStringCookies";
+import { cookies } from "next/headers";
+import Link from "next/link";
 import React from "react";
+import { IoMdAddCircle } from "react-icons/io";
 
 const Products = () => {
+  const cookiesStore = cookies();
+  const strCookies = toStringCookies(cookiesStore);
   return (
     <main className="mt-7 w-full mx-8">
-      <ProductsTable title="لیست محصولات" rowsPerPage={5} />
+      <div className="flex justify-end">
+        <Button
+          color="primary"
+          className="rounded-md flex gap-2 text-sm items-center"
+        >
+          <IoMdAddCircle className="mb-[1px]" size={20} />
+          <Link href="/admin/products/add">اضافه کردن محصول</Link>
+        </Button>
+      </div>
+      <ProductsTable title="لیست محصولات" cookies={strCookies} rowsPerPage={5} />
     </main>
   );
 };
