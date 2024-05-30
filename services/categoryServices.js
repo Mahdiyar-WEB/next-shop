0ceac9bceeaecee9dba96f679905900584681fe4
Mahdiyar marvi <mahdiyar472@gmail.com>
2024-05-30 18:06:15 +0330

    add addNewCategory service

--- services/categoryServices.js
+++ services/categoryServices.js
@@ -3,3 +3,7 @@ const { default: http } = require("./httpService");
 export const getCategories = async () => {
   return http.get("/category/list").then((res) => res.data);
 };
+
+export const addNewCategory = async (data) => {
+  return http.post("/admin/category/add", data).then((res) => res.data);
+};
