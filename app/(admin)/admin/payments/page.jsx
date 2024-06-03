777ff6b9e3cbf58832fcb8826b5f4f4dedd232d0
Mahdiyar marvi <mahdiyar472@gmail.com>
2024-06-03 15:03:27 +0330

    add payments page

---
+++ app/(admin)/admin/payments/page.jsx
@@ -0,0 +1,12 @@
+import AdminOrdersTable from "@/components/AdminOrdersTable";
+import React from "react";
+
+const page = () => {
+  return (
+    <main className="mt-7 w-full mx-8">
+      <AdminOrdersTable title="لیست سفارش کاربران"/>
+    </main>
+  );
+};
+
+export default page;
