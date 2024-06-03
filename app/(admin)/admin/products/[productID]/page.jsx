66a44c090204db00b4b0976c6c1825bab4f7deee
Mahdiyar marvi <mahdiyar472@gmail.com>
2024-06-03 15:03:57 +0330

    remove console.log

--- app/(admin)/admin/products/[productID]/page.jsx
+++ app/(admin)/admin/products/[productID]/page.jsx
@@ -6,7 +6,6 @@ const AdminProduct = ({ params: { productID } }) => {
   const fetchProduct = async () => {
     try {
       const response = await getProductByID(productID);
-      console.log("🚀 ~ fetchProduct ~ response:", response);
     } catch (error) {}
   };
   return <div>AdminProduct</div>;
