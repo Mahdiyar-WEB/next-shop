53e17c2cf4de4ce45a23c88842a82d9485e0a9cc
Mahdiyar marvi <mahdiyar472@gmail.com>
2024-06-03 15:07:44 +0330

    add getPayments service

--- services/paymentServices.js
+++ services/paymentServices.js
@@ -3,3 +3,6 @@ const { default: http } = require("./httpService");
 export const createPayment = async () => {
   return http.post("/payment/create").then((res) => res.data);
 };
+export const getPayments = async () => {
+  return http.get("/admin/payment/list").then((res) => res.data);
+};
