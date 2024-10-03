b1b9a38e6eed27f65192b686fdea8eacad0d924a
Mahdiyar marvi <mahdiyar472@gmail.com>
2024-10-03 14:24:47 +0330

    fix css

--- app/(admin)/admin/categories/edit/[id]/page.jsx
+++ app/(admin)/admin/categories/edit/[id]/page.jsx
@@ -12,7 +12,7 @@ const EditCategory = ({ params: { id } }) => {
   const { data: response, isPending } = useGetCategoryById(id);
   // delete data.category.icon;
   return (
-    <main className="mt-7 w-full mx-auto max-w-2xl border h-fit pb-5 pt-3 px-3 shadow-md rounded-md">
+    <main className="mt-7 w-full mx-auto max-w-2xl h-fit pb-5 pt-3 px-3">
       {response?.data?.category && (
         <EditCategoryDetails categoryDetails={response?.data.category} />
       )}
